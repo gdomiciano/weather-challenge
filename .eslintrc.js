fac0b709afab2f1c48123e45faff0674c45b7c35
Geisy Domiciano <geisydomiciano@gmail.com>
2017-09-11 23:37:02 -0300

    add lint and nuxt config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  rules: {},
-  globals: {}
-}
+    root: true,
+    parser: 'babel-eslint',
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: 'airbnb-base',
+    // required to lint *.vue files
+    plugins: [
+        'html'
+    ],
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,21 @@
 module.exports = {
-  /*
-  ** Build configuration
-  */
-  build: {},
-  /*
-  ** Headers
-  ** Common headers are already provided by @nuxtjs/pwa preset
-  */
-  head: {},
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Customize app manifest
-  */
-  manifest: {
-    theme_color: '#3B8070'
-  },
-  /*
-  ** Modules
-  */
-  modules: [
-    '@nuxtjs/pwa'
-  ]
-}
+
+    build: {},
+
+    head: {
+        title: 'Weather Challenge - Kloeckner',
+        htmlAttrs: {
+            lang: 'en',
+        }
+    },
+
+    loading: { color: '#3B8070' },
+
+    manifest: {
+        theme_color: '#3B8070'
+    },
+
+    modules: [
+        '@nuxtjs/pwa'
+    ],
+};
