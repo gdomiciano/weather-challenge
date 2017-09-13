b0630c21fcf7f9bcb813250c218d89357a652292
Geisy Domiciano <geisydomiciano@gmail.com>
2017-09-12 21:34:26 -0300

    finish manifest colors config; add API key env var

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,24 @@ module.exports = {
         title: 'Weather Challenge - Kloeckner',
     },
 
-    loading: { color: '#3B8070' },
+    loading: { color: '#F3C80F' },
 
     manifest: {
         name: 'Weather Challenge',
         short_name: 'Weather',
         start_url: '.',
         display: 'fullscreen',
-        background_color: '#fff',
+        background_color: '#000000',
         description: '',
         lang: 'en',
         orientation: 'portrait',
-        related_applications: [
-            {
-                platform: 'web',
-            },
-        ],
-        theme_color: 'yellow',
+        theme_color: '#0077AA',
     },
 
     modules: [
         '@nuxtjs/pwa',
     ],
+    env: {
+        API_KEY: 'cecf2cdf0f3ee489ba06aff6db8cb201',
+    },
 };
