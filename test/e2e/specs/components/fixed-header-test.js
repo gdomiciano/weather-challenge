ff869e735da6163d1f5bcea66ee4836c43f2df4a
Geisy Domiciano <geisydomiciano@gmail.com>
2017-09-19 00:00:19 -0300

    add weather info test

--- test/e2e/specs/components/fixed-header-test.js
+++ test/e2e/specs/components/fixed-header-test.js
@@ -10,9 +10,12 @@ module.exports = {
     },
     'Check if header is loaded': (browser) => {
         browser.expect.element('.Header').to.be.present.and.to.be.visible;
+        browser.expect.element('.Header').to.be.a('header');
         browser.expect.element('.Header-brand').to.be.present.and.to.be.visible;
-        browser.expect.element('.Header-brand').text.to.contain('Weather');
+        browser.expect.element('.Header-brand').to.be.a('h1');
+        browser.expect.element('.Header-brand').text.to.equal('Weather');
         browser.expect.element('.Header-icon').to.be.present.and.to.be.visible;
+        browser.expect.element('.Header-icon').to.be.a('svg');
         browser.expect.element('.Header-icon').to.have.attribute('width').equals('50');
         browser.expect.element('.Header-icon').to.have.attribute('height').equals('50');
     },
