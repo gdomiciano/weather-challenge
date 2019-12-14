3919849fdc579ef90e7c2825853f2ac9e4eb3f89
Geisy Domiciano <geisydomiciano@gmail.com>
2019-12-14 14:59:48 +0100

    :construction: add weather info component

---
+++ components/WeatherInfo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h3>{{ cityName }}</h3>
+    <p>temperature:<strong> {{ currentTemp }}</strong></p>
+    <p>
+      <span v-if="hasMinTemp">min:<strong> {{ minTemp }}</strong></span>
+      <span>max:
+        <strong v-if="maxTemp !== ''"> {{ maxTemp }}</strong>
+        <strong v-else> -- </strong>
+      </span>
+    </p>
+  </div>
+</template>
+
+<script>
+import { type } from 'os'
+  export default {
+    name: 'WeatherInfo',
+    props: {
+      cityName: {
+        type: String,
+        required: true
+      },
+      currentTemp: {
+        type: Number,
+        required: true
+      },
+      minTemp: {
+        type: [Number,String],
+        default: '--',
+        validator: function (value) {
+          if(typeof value !== Number) return false
+        }
+      },
+      maxTemp: {
+        type: [Number, String],
+        default: ''
+      }
+    },
+    computed: {
+      hasMinTemp() {
+        return typeof value === Number
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
